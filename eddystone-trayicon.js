9302e613ef2f4cef7ddb95728daab95e1d26b003
Adrian Zalewski <aazalewski@hotmail.com>
2017-09-01 12:28:04 +0200

    Replace tabs with spaces.

--- eddystone-trayicon.js
+++ eddystone-trayicon.js
@@ -4,58 +4,58 @@ const {Menu, Tray, shell} = require('electron');
 // const shell = require('shell');
 
 function EddystoneTrayIcon(opts) {
-	opts = opts || {};
+  opts = opts || {};
 
-	this.trayIcon = new Tray(opts.iconForNotFound);
+  this.trayIcon = new Tray(opts.iconForNotFound);
 
-	if (opts.toolTip) {
-		this.trayIcon.setToolTip(opts.toolTip);
-	}
+  if (opts.toolTip) {
+    this.trayIcon.setToolTip(opts.toolTip);
+  }
 
-	if (opts.iconForPressed) {
-		this.trayIcon.setPressedImage(opts.iconForPressed);
-	}
+  if (opts.iconForPressed) {
+    this.trayIcon.setPressedImage(opts.iconForPressed);
+  }
 
-	this.opts = opts;
-	this.beacons = [];
-	this.refresh();
+  this.opts = opts;
+  this.beacons = [];
+  this.refresh();
 }
 
 EddystoneTrayIcon.prototype.refresh = function () {
-	var contextMenu = null;
+  var contextMenu = null;
 
-	if (this.beacons.length > 0) {
-		this.trayIcon.setImage(this.opts.iconForFound);
+  if (this.beacons.length > 0) {
+    this.trayIcon.setImage(this.opts.iconForFound);
 
-		contextMenu = Menu.buildFromTemplate(this.beacons.map(function (beacon) {
-			return {
-				label: [
-					beacon.url, ',', 'RX Power:',
-					beacon.txPower, ',', 'RSSI',
-					beacon.rssi
-				].join(' '),
-				click: function () {
-					shell.openExternal(beacon.url);
-				}
-			};
-		}));
-	} else {
-		this.trayIcon.setImage(this.opts.iconForNotFound);
+    contextMenu = Menu.buildFromTemplate(this.beacons.map(function (beacon) {
+      return {
+        label: [
+          beacon.url, ',', 'RX Power:',
+          beacon.txPower, ',', 'RSSI',
+          beacon.rssi
+        ].join(' '),
+        click: function () {
+          shell.openExternal(beacon.url);
+        }
+      };
+    }));
+  } else {
+    this.trayIcon.setImage(this.opts.iconForNotFound);
 
-		contextMenu = Menu.buildFromTemplate([{label: 'Not found'}]);
-	}
+    contextMenu = Menu.buildFromTemplate([{label: 'Not found'}]);
+  }
 
-	this.trayIcon.setContextMenu(contextMenu);
+  this.trayIcon.setContextMenu(contextMenu);
 };
 
 EddystoneTrayIcon.prototype.add = function (beacon) {
-	this.beacons.push(beacon);
-	this.refresh();
+  this.beacons.push(beacon);
+  this.refresh();
 };
 
 EddystoneTrayIcon.prototype.clear = function (beacon) {
-	this.beacons = [];
-	this.refresh();
+  this.beacons = [];
+  this.refresh();
 };
 
 module.exports = EddystoneTrayIcon;
--- index.js
+++ index.js
@@ -6,41 +6,41 @@ const EddystoneTrayIcon = require('./eddystone-trayicon');
 const SCANNING_INTERVAL = 1000 * 30;
 
 app.on('window-all-closed', function () {
-	clearTimeout(app.scanTimer);
+  clearTimeout(app.scanTimer);
 
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function () {
-	app.trayIcon = new EddystoneTrayIcon({
-		toolTip: 'Eddystone beacon scanner',
-		iconForNotFound: 'images/lighthouse-grey-16x16.png',
-		iconForFound: 'images/lighthouse-black-16x16.png',
-		iconForPressed: 'images/lighthouse-white-16x16.png'
-	});
+  app.trayIcon = new EddystoneTrayIcon({
+    toolTip: 'Eddystone beacon scanner',
+    iconForNotFound: 'images/lighthouse-grey-16x16.png',
+    iconForFound: 'images/lighthouse-black-16x16.png',
+    iconForPressed: 'images/lighthouse-white-16x16.png'
+  });
 
-	scanner.on('found', function(beacon) {
-		console.log('beacon has been found:', beacon.url);
-		app.trayIcon.add(beacon);
-	});
+  scanner.on('found', function(beacon) {
+    console.log('beacon has been found:', beacon.url);
+    app.trayIcon.add(beacon);
+  });
 
-	scanner.on('updated', function(beacon) {
-		console.log('beacon has been update:', beacon.url);
-		app.trayIcon.add(beacon);
-	});
+  scanner.on('updated', function(beacon) {
+    console.log('beacon has been update:', beacon.url);
+    app.trayIcon.add(beacon);
+  });
 
-	scanner.on('lost', function(beacon) {
-		console.log('beacon has been lost:', beacon);
-		app.trayIcon.clear();
-	});
+  scanner.on('lost', function(beacon) {
+    console.log('beacon has been lost:', beacon);
+    app.trayIcon.clear();
+  });
 
-	scanner.startScanning();
+  scanner.startScanning();
 
-	app.scanTimer = setInterval(function () {
-		console.log('Start beacon scanning');
-		app.trayIcon.clear();
-		scanner.startScanning();
-	}, SCANNING_INTERVAL);
+  app.scanTimer = setInterval(function () {
+    console.log('Start beacon scanning');
+    app.trayIcon.clear();
+    scanner.startScanning();
+  }, SCANNING_INTERVAL);
 });
