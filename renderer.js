6fea34e5d581555f6ed114813cf21dbe65b91861
Patryk Nawolski <patryk.nawolski@netguru.co>
2017-09-01 17:21:30 +0200

    Log all visible beacons with their distances

--- renderer.js
+++ renderer.js
@@ -2,10 +2,51 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var EddystoneBeaconScanner = require('eddystone-beacon-scanner');
+const EddystoneBeaconScanner = require('eddystone-beacon-scanner');
+const beaconsListEl = document.getElementById('beacons-list');
+let beacons = [];
 
 EddystoneBeaconScanner.on('found', function(beacon) {
-  console.log(beacon);
+  const { url } = beacon;
+
+  let foundBeacon = beacons.find(beacon => {
+    if (beacon.url === url) return beacon;
+  });
+
+  if (!foundBeacon) {
+    beacons.push(beacon);
+  } else {
+  }
+
+  displayBeacons();
 });
 
-EddystoneBeaconScanner.startScanning();
+EddystoneBeaconScanner.on('updated', function(beacon) {
+  console.log(beacon);
+  const { distance, url } = beacon;
+
+  let foundBeacon = beacons.find(beacon => {
+    if (beacon.url === url) return beacon;
+  });
+
+  const foundBeaconIndex = beacons.indexOf(foundBeacon);
+
+  beacons[foundBeaconIndex].distance = distance;
+
+  displayBeacons();
+});
+
+EddystoneBeaconScanner.startScanning(true);
+
+function displayBeacons() {
+  beaconsListEl.innerHTML = '';
+
+  beacons.forEach(beacon => {
+    const { distance, url } = beacon;
+    const listEl = document.createElement('li');
+
+    listEl.innerText = `${url} is ${distance} away from here`;
+
+    beaconsListEl.appendChild(listEl);
+  });
+}
